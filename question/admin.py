e416887f3ff990b6b5005eb59528928ba3bfe224
Saiful Islam <saiful.abir20@gmail.com>
2023-01-04 12:23:46 +0600

    choice added to question admin

--- question/admin.py
+++ question/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from question.models import Question, QuestionChoice, Category, CategoryWiseLeaderBoard, StoreAnswer
 
-admin.site.register(Question)
-admin.site.register(QuestionChoice)
+# admin.site.register(Question)
+# admin.site.register(QuestionChoice)
 admin.site.register(Category)
 admin.site.register(CategoryWiseLeaderBoard)
 admin.site.register(StoreAnswer)
+
+class QuestionChoiceInline(admin.TabularInline):
+    model = QuestionChoice
+    fields = ['title', 'is_answer']
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [
+        QuestionChoiceInline
+    ]

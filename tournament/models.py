9816dd7a6a6c0d8b52192364550eaae5c025caa9
Jubayedul <arnok2084@gmail.com>
2023-01-23 13:50:15 +0600

    tournament requrments.txt

--- tournament/models.py
+++ tournament/models.py
@@ -14,8 +14,6 @@ class Tournament(AbstractTimeStamp):
     ]
     title = models.CharField(max_length=245, blank=False, null=False)
     description = models.TextField(blank=True, null=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=False, null=False)
-    question = models.ManyToManyField(Question, blank=True)
     published = models.BooleanField(default=False)
     banner = models.ImageField()
     status = models.CharField(max_length=20, null=False, blank=False, choices=TOURNAMENT_STATUS, default=TOURNAMENT_STATUS[1][1])

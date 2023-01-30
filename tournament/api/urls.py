f375466a86eef40d5ff942098a3f9ace4ad8a9f7
Jubayedul <arnok2084@gmail.com>
2023-01-30 11:44:38 +0600

    tournament Url Update

--- tournament/api/urls.py
+++ tournament/api/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('tournament-list/', TournamentListAPIView.as_view()),
     path('tournament-details/<int:id>', TournamentDetailsAPIView.as_view()),
     path('tournament-question-list/<int:id>/', TournamentQuestionListAPIView.as_view()),
+    path('tournament-store-answer/', StoreTournamentAnswerAPIView.as_view()),
+    path('tournaament-leaderboard-list/<int:tour_id>/', TournamentWiseLeaderBoardListAPIView.as_view()),
 ]
